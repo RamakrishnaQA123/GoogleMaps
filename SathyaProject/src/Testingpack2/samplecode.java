a082d5e4a0562f4dcc1823962214f764674ecfff
LENOVO <LENOVO@LENOVO-PC>
2019-07-26 08:07:17 +0530

    added class samplecode.java

---
+++ SathyaProject/src/Testingpack2/samplecode.java
@@ -0,0 +1,21 @@
+package Testingpack2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class samplecode {
+
+	
+	@Test
+	public void data(){
+	FirefoxDriver driver=new FirefoxDriver();
+	driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+	
+	
+driver.findElement(By.xpath("//*[@id='txtUsername']")).sendKeys("Admin");
+driver.findElement(By.xpath("//*[@id='txtPassword']")).sendKeys("admin123");
+driver.findElement(By.xpath("//*[@id='btnLogin']")).click();
+	}
+	
+}
