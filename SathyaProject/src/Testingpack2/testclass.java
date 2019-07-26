ea2f0b7618f9068a42302a8ef31edc70a98c441b
LENOVO <LENOVO@LENOVO-PC>
2019-07-26 08:12:25 +0530

    added testclass.java

--- SathyaProject/src/Testingpack2/testclass.java
+++ SathyaProject/src/Testingpack2/testclass.java
@@ -1,5 +1,15 @@
 package Testingpack2;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class testclass {
+	
+	
+	@Test
+	public void open(){
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://google.com");
+	}
 
 }
